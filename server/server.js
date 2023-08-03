1a56f7548083c85da69dcfc66b39a3e262943626
joanterm <jo_joanarg@yahoo.com>
2023-08-03 13:06:57 -0400

    Added mongodb and mongoose and connected to database

--- server/server.js
+++ server/server.js
@@ -2,19 +2,25 @@ const express = require("express") //USE EXPRESS
 const server = express()
 server.use(express.json())
 require('dotenv').config() // USE DOTENV
+const mongoose = require("mongoose") //GET MONGOOSE FOR SCHEMAS
 const usersRouter = require("./routes/users-router") //GET USERS ROUTER
 
-//SERVER SET UP
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
-})
-
-// ROUTER
-server.use("/users", usersRouter)
-
+//CONNECT TO DATABASE
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        const PORT = process.env.PORT
+        server.listen(PORT, () => {
+            console.log(`Connected. Listening on port ${PORT}...`) 
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 //SANITY CHECK
 server.get("/", (req, res) => {
     res.send("Express working!")
 })
+
+// ROUTER
+server.use("/users", usersRouter)
