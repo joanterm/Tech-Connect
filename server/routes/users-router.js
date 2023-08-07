053f51f6213581d26be343499a18768ee16bcda2
joanterm <jo_joanarg@yahoo.com>
2023-08-07 15:30:27 -0400

    Added a GET and GET by id functions to the backend

--- server/routes/users-router.js
+++ server/routes/users-router.js
@@ -1,22 +1,45 @@
 const usersRouter = require("express").Router()
+const mongoose = require("mongoose")
 const Users = require("../models/users-model")
 
 usersRouter.get("/", (req, res) => {
-    res.status(200).json({message: "GET ALL USERS"})
+    Users.find({})
+        .then((result) => {
+            res.status(200).json(result)
+        })
+        .catch((error) => {
+            res.status(400).json({message: error.message})
+        })
 })
 
 usersRouter.get("/:id", (req, res) => {
-    res.status(200).json({message: "GET USERS BY ID"})
+    const {id} = req.params
+    if (mongoose.Types.ObjectId.isValid(id)) {
+        Users.findById(id)
+            .then((result) => {
+                if(result == null) {
+                    res.status(404).json({message: "This ID doesn't exist"})
+                } else {
+                    res.status(200).json(result)
+                }
+            })
+            .catch((error) => {
+                res.status(400).json({message: error.message})
+            })
+    }
+    else {
+        res.status(404).json({message: "This ID is not in a valid form"})
+    }
 })
 
 usersRouter.post("/", (req, res) => {
-    const { name, about, social } = req.body
-    Users.create({ name, about, social })
+    const {name, about, social} = req.body
+    Users.create({name, about, social})
         .then((result) => {
             res.status(200).json(result);
         })
         .catch((error) => {
-            res.status(400).json({ message: error.message });
+            res.status(400).json({message: error.message});
         })
 })
 
