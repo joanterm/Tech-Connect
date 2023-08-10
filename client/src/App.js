a6462ba6e22e7ff6f62b3a0d46026af0ca6e0645
joanterm <jo_joanarg@yahoo.com>
2023-08-10 15:36:57 -0400

    Added a simple form and POST request to the client side

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,11 @@ import axios from "axios"
 function App() {
   const URL = "http://localhost:9000/users"
   const [usersData, setUsersData] = useState([])
+  const [formData, setFormData] = useState({
+    name: "",
+    about: "",
+    social: ""
+  })
   
   useEffect(() => {
     axios
@@ -18,11 +23,65 @@ function App() {
     })
   }, [])
 
+  const postUserInfo = (userInfo) => {
+    axios
+    .post(URL, userInfo)
+    .then((response) => {
+      setUsersData([
+        ...usersData,
+        response.data
+      ])
+    })
+    .catch((error) => {
+      console.log("POST ERROR", error)
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    postUserInfo({
+      name: formData.name,
+      about: formData.about,
+      social: formData.social
+    })
+  }
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div className="App">
       <h1>TESTING REACT</h1>
+      <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input 
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <label htmlFor="about">About:</label>
+          <input 
+            type="text"
+            name="about"
+            value={formData.about}
+            onChange={handleChange}
+          />
+          <label htmlFor="about">Social:</label>
+          <input 
+            type="text"
+            name="social"
+            value={formData.social}
+            onChange={handleChange}
+          />
+          <button className="submit-button">Submit</button>
+      </form>
       {usersData.map((item) => (
-        <div>
+        <div key={item._id}>
           <p>Name: {item.name}</p>
           <p>About: {item.about}</p>
           <p>Social: {item.social}</p>
