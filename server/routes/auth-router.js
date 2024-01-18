8abf2fdb9473c51bcc455795a6d84452315b06a6
joanterm <jo_joanarg@yahoo.com>
2024-01-18 14:22:29 -0500

    Installed and generated a json web token

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,8 +1,13 @@
 const authRouter = require("express").Router()
-const mongoose = require("mongoose")
 const Auth = require("../models/auth-model")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const {isEmailTaken} = require("../middleware/auth-middleware")
+
+//JSON TOKEN
+const generateToken = (id) => {
+    return jwt.sign({_id: id}, process.env.JWT_SECRET, {expiresIn: "5h"})
+}
 
 authRouter.post("/login", (req, res) => {
     res.status(200).json({message: "logged in"})
@@ -12,8 +17,9 @@ authRouter.post("/signup", isEmailTaken, (req, res, next) => {
     const {email, password} = req.body
     const hashedPassword = bcrypt.hashSync(password, 10)
     Auth.create({email, password: hashedPassword})
-    .then((user) => {
-        res.status(201).json({email, user})
+    .then((result) => {
+        const jwtToken = generateToken(result._id)
+        res.status(201).json({email: result.email, jwtToken: jwtToken})
     })
     .catch(next)
 })
